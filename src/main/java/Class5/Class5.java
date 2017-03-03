5b32fa828f612c9607bf63592f51d9fdc457536e
layash <layashpiya87@gmail.com>
2017-03-03 18:18:43 +0545

    kai xaina

---
+++ src/main/java/Class5/Class5.java
@@ -0,0 +1,15 @@
+package Class5;
+
+/**
+ * Created by layash on 2/23/2017.
+ */
+public class Class5 {
+    public static void main(java.lang.String[] args) {
+        StringBuilder str= new StringBuilder("I am a string builder");
+        System.out.println("str="+str);
+        Class5 s=new Class5();
+        System.out.println(s.toString());
+        }
+
+
+}
---
+++ src/main/java/class6/Array.java
@@ -0,0 +1,19 @@
+package class6;
+
+import java.util.Scanner;
+
+/**
+ * Created by layash on 2/27/2017.
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("input string");
+        String value = input.nextLine();
+        System.out.println("insert value"+value);
+
+
+    }
+    }
+
